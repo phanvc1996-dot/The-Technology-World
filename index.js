d45e25f6f7fc71c2bebcfaa94ad3299f9c0f9a8c
Phan Việt Cường <PhanVC1996@gmail.com>
2026-05-21 21:44:52 +0700

    finished the sixth video

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const addressbtn = document.querySelector("#address-form");
 const addressClose = document.querySelector("#Close");
+
 console.log(addressbtn);
 console.log(addressClose);
 
@@ -9,4 +10,30 @@ addressbtn.addEventListener("click", function () {
 
 addressClose.addEventListener("click", function () {
   document.querySelector(".address-form").style.display = "none";
+});
+
+// Slider ===============================================================
+const rightbtn = document.querySelector(".fa-chevron-right");
+const leftbtn = document.querySelector(".fa-chevron-left");
+const imgNumber = document.querySelectorAll(".slider-content-left-top img");
+console.log(imgNumber.length);
+
+let index = 0;
+
+rightbtn.addEventListener("click", function () {
+  index = index + 1;
+  if (index > imgNumber.length - 1) {
+    index = 0;
+  }
+  document.querySelector(".slider-content-left-top").style.right =
+    index * 100 + "%";
+});
+
+leftbtn.addEventListener("click", function () {
+  index = index - 1;
+  if (index <= 0) {
+    index = imgNumber.length - 1;
+  }
+  document.querySelector(".slider-content-left-top").style.right =
+    index * 100 + "%";
 });
